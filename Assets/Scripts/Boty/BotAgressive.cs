83af5b2e1470f320b65d1de2d03a8b207db1f6b1
Th3NiKo <niko.pachala@onet.eu>
2018-05-27 22:47:28 +0200

    Add bot and basic functions

---
+++ Assets/Scripts/Boty/BotAgressive.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotAgressive : MonoBehaviour {
+
+	GameManager gameManager;
+	public int playerIndex = 10;
+	
+	//Timers
+	float timer = 0.0f;
+	float turnTime;
+	
+	void Start () {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		turnTime = gameManager.GetTurnTime();
+	}
+	
+	
+	void Update () {
+		if(timer > turnTime){
+			//Make Move
+			//1.Check for closest enemy
+			float distance = 9999;
+			float index = 0;
+			for(int i = 10; i <= 30; i+=10)
+			{
+				if(i != playerIndex){
+					float temp = Vector2.Distance(new Vector2(gameManager.players[(i / 10) - 1].x,gameManager.players[(i / 10) - 1].y)
+												   ,new Vector2(gameManager.players[(playerIndex / 10) - 1].x,gameManager.players[(playerIndex / 10) - 1].y));
+					if(distance < temp){
+						distance = temp;
+						index = i;						   	
+					}
+				}
+			}
+			//2.Check if u are close enough
+			bool isClose;
+			if(distance <= 2){
+				isClose = true;
+			} else{
+				isClose = false;
+			}
+
+
+			timer = 0.0f;
+		}
+
+	timer += Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Boty/BotRandom.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+Bot check if can move or place bomb if can do it. If cant rotate random direction.
+
+ */
+
+public class BotRandom : MonoBehaviour {
+
+	GameManager gameManager;
+	public int playerIndex = 10;
+
+	//Timers
+	float timer = 0.0f;
+	float turnTime;
+	
+	void Start () {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		turnTime = gameManager.GetTurnTime();
+	}
+	
+	
+	void Update () {
+		if(timer > turnTime){
+			//Make random move
+			int randomValue = Random.Range(1,6);
+			if(randomValue < 5){
+				if(gameManager.CanMoveForward(playerIndex)){
+					gameManager.MoveForward(playerIndex);
+				} else {
+					randomValue = Random.Range(1,2);
+					if(randomValue == 1){
+						gameManager.RotateClockwise(playerIndex);
+					} else {
+						gameManager.RotateCounterClockwise(playerIndex);
+					}
+				}
+			} else {
+				if(gameManager.CanMoveForward(playerIndex)){
+					gameManager.PlaceBomb(playerIndex);
+				} else {
+					randomValue = Random.Range(1,2);
+					if(randomValue == 1){
+						gameManager.RotateClockwise(playerIndex);
+					} else {
+						gameManager.RotateCounterClockwise(playerIndex);
+					}
+				}
+			}
+			timer = 0.0f;
+		}
+		
+		timer += Time.deltaTime;
+		}
+		
+}
+
